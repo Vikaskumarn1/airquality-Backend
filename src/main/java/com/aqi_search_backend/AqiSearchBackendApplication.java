e208affa53e97c51bf852815263f9c69b95ed032
Vikaskumarn1 <143833684+Vikaskumarn1@users.noreply.github.com>
2025-11-26 16:21:56 +0530

    Update AqiSearchBackendApplication.java

--- src/main/java/com/aqi_search_backend/AqiSearchBackendApplication.java
+++ src/main/java/com/aqi_search_backend/AqiSearchBackendApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication  // Marks this as the main Spring Boot app
-@EnableCaching          // Enables caching for faster repeated responses
+@SpringBootApplication  
+@EnableCaching          
 public class AqiSearchBackendApplication {
 
     public static void main(String[] args) {
